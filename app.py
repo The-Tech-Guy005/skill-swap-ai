83dec71712e9bdc9cb2fe2b613c7cc28f43a61ad
The-Tech-Guy005 <hridayarora2005@gmail.com>
2026-07-07 01:57:11 +0530

    Final improvements and bug fixes

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
 import markdown
 from pypdf import PdfReader
+import pdfplumber
 
 app = Flask(__name__)
 
@@ -47,10 +48,19 @@ def ask():
     5. Career Recommendations
 
     Do NOT repeat the ATS score again in the main response.
+    Be consistent with ATS scoring. Give similar scores for similar resumes.
     """
 
-    response = model.generate_content(prompt)
-    answer = markdown.markdown(response.text)
+    try:
+        response = model.generate_content(prompt)
+        answer = markdown.markdown(response.text)
+
+    except Exception:
+         answer = """
+    <h2>⚠️ Gemini API Error</h2>
+    <p>The AI service is temporarily unavailable or the quota has been exceeded.</p>
+    <p>Please try again later.</p>
+    """
 
     return jsonify({
         "answer": answer
@@ -61,12 +71,13 @@ def upload():
 
     file = request.files["resume"]
 
-    reader = PdfReader(file)
-
     text = ""
 
-    for page in reader.pages:
-        text += page.extract_text()
+    with pdfplumber.open(file) as pdf:
+        for page in pdf.pages:
+            page_text = page.extract_text()
+            if page_text:
+                text += page_text + "\n"
 
     return jsonify({
         "text": text
@@ -74,5 +85,5 @@ def upload():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
 
